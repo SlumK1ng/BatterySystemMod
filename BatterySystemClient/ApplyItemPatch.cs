a22bc87b4d7d47fad576b57022890e2a0dac9673
Birger E <bibbevidio@gmail.com>
2024-08-08 17:40:17 +0200

    remove slot checks

--- BatterySystemClient/ApplyItemPatch.cs
+++ BatterySystemClient/ApplyItemPatch.cs
@@ -30,25 +30,11 @@ namespace BatterySystem
 		{
 			if (BatterySystemPlugin.InGame() && __instance.ContainedItem.ParentRecursiveCheck(BatterySystemPlugin.localInventory.Equipment.GetSlot(EquipmentSlot.Headwear).ParentItem))
 			{
-				if (BatterySystem.IsInSlot(__instance.ContainedItem, BatterySystemPlugin.localInventory.Equipment.GetSlot(EquipmentSlot.Earpiece)))
-				{
-                    HeadsetBatteries.SetEarPieceComponents();
-					return;
-				}
-				else if (BatterySystem.IsInSlot(__instance.ParentItem, BatterySystemPlugin.localInventory.Equipment.GetSlot(EquipmentSlot.Headwear)))
-				{ //if item in headwear slot applied
-					NightVisionBatteries.SetHeadWearComponents();
-					return;
-				}
-				else if (BatterySystem.IsInSlot(__instance.ContainedItem, Singleton<GameWorld>.Instance?.MainPlayer.ActiveSlot))
-				{ // if sight is removed and empty slot is applied, then remove the sight from sightdb
-					TacticalDeviceBatteries.CheckDeviceIfDraining();
-					SightBatteries.CheckSightIfDraining();
-					return;
-				}
-                HeadsetBatteries.SetEarPieceComponents();
-                NightVisionBatteries.SetHeadWearComponents();
-			}
+			
+			HeadsetBatteries.SetEarPieceComponents();
+			NightVisionBatteries.SetHeadWearComponents();
+			TacticalDeviceBatteries.CheckDeviceIfDraining();
+			SightBatteries.CheckSightIfDraining();
 		}
 	}
 }
